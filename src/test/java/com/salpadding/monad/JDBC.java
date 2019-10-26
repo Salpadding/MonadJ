bb9ef64e8eaa07922bd60ed37975fc10ade47ad0
zhuyingjie <m6567fc@outlook.com>
2019-10-26 17:04:25 +0800

    remove gbk encoded text

--- src/test/java/com/salpadding/monad/JDBC.java
+++ src/test/java/com/salpadding/monad/JDBC.java
@@ -6,25 +6,20 @@ public class JDBC {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/test";
 
-    //第三步：说明数据库的认证账户及密码
     static final String USER = "root";
     static final String PASS = "123456";
     public static void main(String[] args) {
-        //第四步：注册JDBC驱动
         try {
             Class.forName(JDBC_DRIVER);
         } catch (ClassNotFoundException e) {
-            //这里会发生类没有找到的异常！
             e.printStackTrace();
             return;
         }
-        //第五步：获得数据库连接
         Connection connection = null;
         Statement statement = null;
         ResultSet rs = null;
         try {
             connection =  DriverManager.getConnection(DB_URL,USER,PASS);
-            //第六步：执行查询语句
             statement = connection.createStatement();
             String sql = "SELECT * FROM crawler_article";
             rs = statement.executeQuery(sql);
--- src/test/java/com/salpadding/monad/JDBCFunctional.java
+++ src/test/java/com/salpadding/monad/JDBCFunctional.java
@@ -6,7 +6,6 @@ public class JDBCFunctional {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://localhost:3306/test";
 
-    // 第三步：说明数据库的认证账户及密码
     static final String USER = "root";
     static final String PASS = "123456";
 
